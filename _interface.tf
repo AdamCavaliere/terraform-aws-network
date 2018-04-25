8d857c59a6ddea655d8c9016bdfa13b703914a8c
AdamCavaliere <adam.cavaliere@gmail.com>
2018-04-25 08:15:43 -0500

    Update _interface.tf

--- _interface.tf
+++ _interface.tf
@@ -1,19 +1,19 @@
 variable "region" {
-  default     = ""
+  default     = "us-east-2"
   description = "The default AZ to provision to for the provider"
 }
 
 variable "vpc_cidr_block" {
-  default     = ""
+  default     = "10.0.0.0/16"
   description = "The default CIDR block for the VPC demo"
 }
 
 variable "subnet_cidr_block" {
-  default     = ""
+  default     = "10.0.1.0/24"
   description = "The default CIDR block for the subnet demo"
 }
 
 variable "subnet_availability_zone" {
-  default     = ""
+  default     = "us-east-2c"
   description = "The default AZ for the subnet"
 }
